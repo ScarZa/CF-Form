0c9dba9174f7c4dcff33631a281e322cfd6c2de7
com <com@com69>
2020-10-28 13:16:16 +0700

    update V.2.1 menu แรกรับ 28/10/63

--- font/JScontent/assSlideMenuIPD.js
+++ font/JScontent/assSlideMenuIPD.js
@@ -68,7 +68,7 @@ function AssSlideMenuIPD(content, id = null) {
                                         $("a#cigarette").attr("onclick","#");
                                         $("a#cgi_menu").attr("onclick","$('#head-detial').show();AssCGI('#index_content');");
                                         $("a#culture").attr("onclick", "$('#head-detial').show();AssCulture('#index_content');");
-                                        $("a#FR").attr("onclick", "popup('../../PCT/front/content/first_ipd.html?id="+$.cookie('an')+"', popup, 1440, 900);");
+                                        $("a#FR").attr("onclick", "popup('../../PCT/front/content/first_ipd.html?id="+$.cookie('an')+"?recorder="+$.cookie('user')+"', popup, 1440, 900);");
                                         //$("a#smi-v").attr("onclick","#");
                                         $("a#depress").attr("onclick","$('#head-detial').show();AssDepress('#index_content');");
                                         $("a#C-case").attr("onclick","$('#head-detial').show();AssC_Case('#index_content');");
